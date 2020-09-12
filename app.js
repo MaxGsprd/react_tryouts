b851cbe2b802fbfa8df9811ae4d7a53af39d18fd
MaxGsprd <max.gaspard@outlook.fr>
2020-09-12 19:45:09 +0200

    function render created

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-function Jedi(name, rank, saberColor) {
-    this.name = name;
-    this.rank = rank;
-    this.saberColor = saberColor;
-    this.forceShield = function() {
-        console.log('force shield bziiiit');
-    }
+let n = 0;
+
+function render() {
+    const title = React.createElement('h1', {},'React introduction ',
+    React.createElement('span', {}, n));
+    ReactDOM.render(title, document.querySelector('#app'));
 }
 
+render();
 
-let quigon = new Jedi('quigon','master','green');
+window.setInterval(() => {
+    n++
+    render();
+}, 1000);
 
-function JediMaster(name, rank, saberColor) {
-    Jedi.call(this, name, rank, saberColor);
-    this.padawan = 'anakin';
-}
-
-let obiwan = new JediMaster('obiwan', 'master', 'blue');
