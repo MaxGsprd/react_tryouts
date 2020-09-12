21f00475032ec603ca92a94870fff8f91dbeda65
MaxGsprd <max.gaspard@outlook.fr>
2020-09-12 22:03:29 +0200

    jsx syntax added

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 let n = 0;
+/**
+ * 
+ * @param {number} n - convert number to string and add 3 zero at the begining of the string
+ */
+function numberFormat(n) {
+    return n.toString().padStart(3, '0');
+}
 
+/**
+ * render the DOM
+ * see React Doc for more info on React.fragment (used here not to have a div in a div)
+ * see the key prop. for the items identification & sorting & better performance
+ */
 function render() {
-    const title = React.createElement('h1', {},'React introduction ',
-    React.createElement('span', {}, n));
-    ReactDOM.render(title, document.querySelector('#app'));
+    const arrItems = [
+        'task 1',
+        'task 2',
+        'task 3'
+    ];
+    const content = <React.Fragment>
+        <h1 className="title">
+            Hello Mario <span>{numberFormat(n)}</span>
+        </h1>
+        <ul>
+            {arrItems.map((item, k) => <li key={k}>{item}</li>)}
+        </ul>
+    </React.Fragment>
+  
+    ReactDOM.render(content, document.querySelector('#app'));
 }
 
 render();
