f6f2b30f093b55792217f8c321cc2188f4c78bac
MaxGsprd <max.gaspard@outlook.fr>
2020-09-13 20:41:19 +0200

    my firsts components created

--- app.js
+++ app.js
@@ -1,39 +1,90 @@
-let n = 0;
-/**
- * 
- * @param {number} n - convert number to string and add 3 zero at the begining of the string
- */
-function numberFormat(n) {
-    return n.toString().padStart(3, '0');
+class Welcome extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>{this.props.name}</h1>
+            <p>{this.props.children}</p>
+            <hr/>
+        </div>);
+    }
 }
 
-/**
- * render the DOM
- * see React Doc for more info on React.fragment (used here not to have a div in a div)
- * see the key prop. for the items identification & sorting & better performance
- */
-function render() {
-    const arrItems = [
-        'task 1',
-        'task 2',
-        'task 3'
-    ];
-    const content = <React.Fragment>
-        <h1 className="title">
-            Hello Mario <span>{numberFormat(n)}</span>
-        </h1>
-        <ul>
-            {arrItems.map((item, k) => <li key={k}>{item}</li>)}
-        </ul>
-    </React.Fragment>
-  
-    ReactDOM.render(content, document.querySelector('#app'));
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date : new Date()};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componantWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Il est {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );
+    }
 }
 
-render();
+class Increment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {n: props.start}
+    }
 
-window.setInterval(() => {
-    n++
-    render();
-}, 1000);
+    componentDidMount () {
+        this.timerID = setInterval(
+            () => this.incrementStart(),
+            1000);
+    }
 
+    componantWillUnmount () {
+        clearInterval(this.timerID);
+    }
+
+    incrementStart() {
+       this.setState((state, props) => ({n: state.n + props.step}))
+    }
+
+    render() {
+
+        return (
+            <React.Fragment>
+                <h2>Timer : {this.state.n}</h2>
+            </React.Fragment>
+        )
+    }
+}
+
+Increment.defaultProps = {
+    start: 0,
+    step: 1
+}
+
+function App () {
+    return (
+    <React.Fragment>
+        <Welcome name="Luffy">Captain</Welcome>
+        <Welcome name="Zoro">First-mate</Welcome>
+        <Clock/>
+        <Increment start={10}/>
+        <Increment start={0} step={10}/>
+    </React.Fragment>);
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
