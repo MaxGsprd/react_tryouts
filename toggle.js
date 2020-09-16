9c663588f05d9bed6284b08f4d5ed2fead70b468
MaxGsprd <max.gaspard@outlook.fr>
2020-09-16 20:47:56 +0200

    added second functions to toggle.js

--- toggle.js
+++ toggle.js
@@ -8,7 +8,6 @@ class Toggle extends React.Component {
       this.handleClick = this.handleClick.bind(this);
     }
     
-
     handleClick() {
         this.setState(state => ({
         isToggleOn: !state.isToggleOn
@@ -19,7 +18,7 @@ class Toggle extends React.Component {
       return (
         <button onClick={this.handleClick}>
           {this.state.isToggleOn ? 'ON' : 'OFF'}
-        </button>
+        </button> 
       );
     }
   }
@@ -29,7 +28,37 @@ class Toggle extends React.Component {
     document.getElementById('root')
   );
 
+//  following code to delete
+  class Toggle extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {isToggleOn: true};
   
+      // Cette liaison est nécéssaire afin de permettre
+      // l'utilisation de `this` dans la fonction de rappel.
+      this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+        this.setState(state => ({
+        isToggleOn: !state.isToggleOn
+      }));
+    }
+  
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button> 
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root')
+  );
 
-      
+
+
 
